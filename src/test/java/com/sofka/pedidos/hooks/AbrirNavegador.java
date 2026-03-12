b751616b0c05d908fe238e9789e9bdfa82b746e4
nico-salsa <nicolasvargasflores6@gmail.com>
2026-03-12 17:36:33 -0500

    feat: add util/Constantes.java URL and Mesa, add hooks

---
+++ src/test/java/com/sofka/pedidos/hooks/AbrirNavegador.java
@@ -0,0 +1,27 @@
+package com.sofka.pedidos.hooks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Open;
+
+public class AbrirNavegador implements Task {
+
+    private String url;
+
+    public AbrirNavegador(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+            Open.url(url)
+        );
+    }
+
+    public static AbrirNavegador AbrirUrl(String url) {
+        return Tasks.instrumented(AbrirNavegador.class, url);
+    }
+    
+}
